5d8487ec81118df7d5db42891b6eb704eef3030f
Matthew Ziskin <mattaziskin@gmail.com>
2023-01-22 16:31:21 -0700

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards(name){
+    const message = [];
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+    console.log(message);
+return message;
+}
+
+
+
+function countDown(number){
+    for(let i = 0; i <= number; number--) {
+        console.log(number);
+    }
+
+}
+
